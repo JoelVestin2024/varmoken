d1bf7c27698424e6f747a5ddaed58c7a85a06446
JoelVestin2024 <joel.vestin06@skola.stenungsund.se>
2025-01-16 13:27:55 +0100

    :)

---
+++ js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.varmoken = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(Drupal);
